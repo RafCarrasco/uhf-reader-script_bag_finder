f312c8bd96d1ca61512c85b6c225ead5f520585f
RafCarrasco <rafaelcarrasco304@gmail.com>
2025-05-21 23:51:34 -0300

    script for access point

--- src/server.js
+++ src/server.js
@@ -1,31 +1,21 @@
 const net = require("net");
-const fs = require("fs");
 
-const HOST = "0.0.0.0"; 
-const PORT = 5000;    
+const client = new net.Socket();
+client.connect(5000, "192.168.0.10", () => {
+  console.log("Conectado ao leitor");
 
-const server = net.createServer((socket) => {
-    const clientIP = socket.remoteAddress;
-    console.log(" Leitor conectado:", clientIP);
-
-    socket.on("data", (data) => {
-        const hex = data.toString("hex").toUpperCase();
-        const timestamp = new Date().toISOString();
-
-        console.log(`[${timestamp}] Dados recebidos: ${hex}`);
-
-        fs.appendFileSync("leituras.txt", `[${timestamp}] ${hex}\n`);
-    });
-
-    socket.on("end", () => {
-        console.log("🔌 Leitor desconectado:", clientIP);
-    });
-
-    socket.on("error", (err) => {
-        console.error("Erro:", err.message);
-    });
+  const command = Buffer.from([0x04, 0x00, 0x27, 0x01]); 
+  client.write(command);
 });
 
-server.listen(PORT, HOST, () => {
-    console.log(`Servidor aguardando conexões em ${HOST}:${PORT}`);
+client.on("data", (data) => {
+  console.log("Dados recebidos:", data.toString("hex"));
+});
+
+client.on("error", (err) => {
+  console.error("Erro:", err.message);
+});
+
+client.on("close", () => {
+  console.log("Conexão encerrada");
 });
