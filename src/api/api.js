2bacab57ee05a7eeb58252f338e411de36203382
Rafael Carrasco <rafaelcarrasco304@gmail.com>
2025-10-04 09:21:12 -0300

    bag readings

--- src/api/api.js
+++ src/api/api.js
@@ -7,8 +7,6 @@ import userRoutes from './routes/userRoutes.js';
 import rfidTagRoutes from "./routes/rfidTagRoutes.js";
 import readingRoutes from "./routes/readingRoutes.js";
 
-
-
 const app = express();
 
 app.use(cors());
